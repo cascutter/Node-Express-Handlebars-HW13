e39075be32fec74158d527b690bfd16a4169fc02
cascutter <cascutter13@gmail.com>
2020-10-17 16:24:35 -0500

    Inserted seed data into table

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+-- Seed data for burgers table --
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES 
+("Beets of Burden Burger"),
+("The Human Polenta-pede Burger"),
+("Don't Get Creme Fraiche With Me Burger"),
+("Chard to a Crisp Burger"),
+("Fig-eta 'Bout It Burger")
