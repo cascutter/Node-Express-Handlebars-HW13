c9317e26c9289965a53ba497a0b2b788345a67a5
cascutter <cascutter13@gmail.com>
2020-10-17 17:15:33 -0500

    orm methods written; **untested**

---
+++ config/orm.js
@@ -0,0 +1,39 @@
+// Importing connection.js
+const connection = require("..config/connection.js");
+
+// Methods to retrieve data and store data in burgers_db
+let orm = {
+    // Selects all data from burgers table
+    selectAll: function(tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // Inserts data to burgers table
+    insertOne: function(table, value, cb) {
+        let queryString = "INSERT INTO " + table + " SET ?";
+        connection.query(queryString, value, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // Updates data in burger table
+    updateOne: function(table, condition, id, cb) {
+        let queryString = "UPDATE " + table + " SET " + condition + " WHERE id = ?";
+        connection.query(queryString, id, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+}
+
+// Export ORM object
+module.exports = orm;
