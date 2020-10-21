e36c068ad749676f4272a348ff2c1f94cbd76493
cascutter <cascutter13@gmail.com>
2020-10-21 12:19:44 -0500

    Updated require

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 // Import the model (burger.js)
-var burger = require("../models/burger");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
