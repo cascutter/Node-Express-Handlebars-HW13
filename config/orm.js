ff5e954ae40cd7675ad4b51b9f264df46eeb406b
cascutter <cascutter13@gmail.com>
2020-10-21 13:12:38 -0500

    Fixed const variable

--- config/orm.js
+++ config/orm.js
@@ -3,9 +3,9 @@ const connection = require("../config/connections.js");
 
 // Helper function for SQL syntax.
 function printQuestionMarks(num) {
-    const arr = [];
+    let arr = [];
 
-    for (const i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         arr.push("?");
     }
 
@@ -14,11 +14,11 @@ function printQuestionMarks(num) {
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-    const arr = [];
+    let arr = [];
 
     // loop through the keys and push the key/value as a string int arr
-    for (const key in ob) {
-        const value = ob[key];
+    for (let key in ob) {
+        let value = ob[key];
         // check to skip hidden properties
         if (Object.hasOwnProperty.call(ob, key)) {
             // if string with spaces, add quotations
@@ -34,9 +34,9 @@ function objToSql(ob) {
 }
 
 // Object for all our SQL statement functions.
-const orm = {
+let orm = {
     selectAll: function(tableInput, cb) {
-        const queryString = "SELECT * FROM " + tableInput + ";";
+        let queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
@@ -45,7 +45,7 @@ const orm = {
         });
     },
     insertOne: function(table, cols, vals, cb) {
-        const queryString = "INSERT INTO " + table;
+        let queryString = "INSERT INTO " + table;
 
         queryString += " (";
         queryString += cols.toString();
@@ -66,7 +66,7 @@ const orm = {
     },
 
     updateOne: function(table, objColVals, condition, cb) {
-        const queryString = "UPDATE " + table;
+        let queryString = "UPDATE " + table;
 
         queryString += " SET ";
         queryString += objToSql(objColVals);
@@ -84,7 +84,7 @@ const orm = {
     },
 
     deleteOne(table, condition, cb) {
-        var queryString = "DELETE FROM " + table;
+        let queryString = "DELETE FROM " + table;
         queryString += " WHERE ";
         queryString += condition;
 
