388db7d529e7574a2d642764eba19eebbb02e4d5
cascutter <cascutter13@gmail.com>
2020-10-21 12:41:01 -0500

    Changed burgers array

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,7 +8,7 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
         var hbsObject = {
-            burgers_db: data,
+            burgers: data,
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
