145af8b1da560becc7813c016f92609a5009cc4c
cascutter <cascutter13@gmail.com>
2020-10-21 08:37:09 -0500

    restructured variables

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,28 @@
 // Import ORM
 const orm = require("../config/orm.js");
 
-let burger = {
+// The variables cols and vals are arrays.
+const burger = {
     selectAll: function(cb) {
         orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
-    insertOne: function(value, cb) {
-        orm.insertOne("burgers", value, cb, function(res) {
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
-    updateOne: function(condition, id, cb) {
-        orm.updateOne("burgers", condition, id, cb, function(res) {
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
             cb(res);
         });
-    }
+    },
+    deleteOne: function(condition, cb) {
+        orm.deleteOne("burgers", condition, function(res) {
+            cb(res);
+        });
+    },
 };
 
 // Export database functions for burgers_controller.js
