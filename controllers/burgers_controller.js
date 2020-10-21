d6b39d85aad29c5702b17d9e12dc70cb48ee824f
cascutter <cascutter13@gmail.com>
2020-10-21 08:35:37 -0500

    Fixed console log and typos

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,34 +10,43 @@ router.get("/", function(req, res) {
         let hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
-        res.render("index". hbsObject);
+        console.log("hbsObject inside of routes: ", hbsObject);
+        res.render("index", hbsObject);
     });
 });
 
 // Insert new burger data
+// post a new burger
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-        res.json({ id:result.insertId })
+    burger.insertOne(req.body.burger_name, function(result) {
+        console.log(result);
+        res.json(result);
     });
 });
 
 // Set burger "devoured" boolean to true
-router.put("/api/burgers/:id", function(req, res) {
-    let condition = "id = " + req.params.id;
-    console.log("condition", condition);
-    burger.update(
-        {
-            devoured: req.body.devoured
-        },
-        condition,
-        function(result) {
-            if (result.changedRows === 0) {
-                return res.status(404).end();
-            }
-            res.status(200).end();
+router.put("/api/burgers/devoured/:id", function(req, res) {
+    const condition = `id = ${req.params.id};`;
+    const boolean = req.body.devoured;
+
+    burger.updateOne(boolean, condition, function(result) {
+        if (result.changedRows === 0) {
+            //if no rows were changed, the ID must not exist so 404
+            return res.status(404).end();
         }
-    );
+        res.status(202).end();
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
 });
 
 // Export routes for server.js
