c4e47c50d8229ee412036797ba5d782ecae2806e
cascutter <cascutter13@gmail.com>
2020-10-21 10:16:55 -0500

    Updated connection

--- config/connections.js
+++ config/connections.js
@@ -1,21 +1,20 @@
 // Sets up connection to MySQL
-require("dotenv").config();
 const mysql = require("mysql");
+require("dotenv").config();
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
-        port: 3306,
         user: "root",
-        password: "G0disD3ad!",
+        password: process.env.PASS,
         database: "burgers_db"
     });
 };
 
 // Makes connection
-connection.connect(function (err) {
+connection.connect(function(err) {
     if (err) {
         console.log("Error connecting: " + err.stack);
         return;
